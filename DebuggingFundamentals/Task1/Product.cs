e7b786950a4308a18a2eb185be76a720b2738062
dakazia <davudova.jul@gmail.com>
2021-04-01 01:22:04 +0300

    Fixed the methods so that the tests run successfully.

---
+++ DebuggingFundamentals/Task1/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Product
+    {
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        private string Name { get; }
+
+        private double Price { get; }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Product);
+        }
+
+        public bool Equals(Product other)
+        {
+            return other != null &&
+                   Name == other.Name &&
+                   Math.Abs(Price - other.Price) < double.Epsilon;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Price);
+        }
+    }
+}
